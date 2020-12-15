cd9a48231842ca8fcde4e0e3d61d6050b2eeb92c
tiaeastwood <teastwood88@gmail.com>
2020-12-15 15:55:22 +0000

    get all users endpoint added

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,15 @@
-const { selectUserByUsername } = require('../models/users');
+const { selectUserByUsername, fetchAllUsers } = require('../models/users');
 
 exports.getUser = async (req, res) => {
   const { username } = req.params;
   const user = await selectUserByUsername(username);
   res.send({ user });
 };
+
+exports.getAllUsers = async (req, res) => {
+  const users = await fetchAllUsers();
+  return res.send({ users });
+};
+
+
+
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 const usersRouter = require('express').Router();
-const { getUser } = require('../controllers/users');
+const { getUser, getAllUsers } = require('../controllers/users');
 const { withErrorHandling, methodNotAllowed } = require('../errors');
 
 usersRouter
   .route('/:username')
   .get(withErrorHandling(getUser))
   .all(methodNotAllowed);
+
+  usersRouter
+  .route('/')
+  .get(withErrorHandling(getAllUsers))
+  .all(methodNotAllowed);
+
 
 module.exports = usersRouter;
